fea09cde86ee1170d671d9dec45e7b4de92f29d6
Tomasz Gostek <tomasz.gostek@uj.edu.pl>
2014-12-06 15:45:30 +0100

    Rotated

--- Constructive Solid Geometry/src/constructive/solid/geometry/Rotated.java
+++ Constructive Solid Geometry/src/constructive/solid/geometry/Rotated.java
@@ -6,18 +6,30 @@
 
 package constructive.solid.geometry;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 /**
  *
  * @author tomaszgostek
  */
 public class Rotated implements Shape{
     
+    private Shape shape;
+    private float x, y, angle;
+    
     public Rotated(float x, float y, float angle, Shape shape) {
-        
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.shape = shape;
     }
 
     @Override
     public boolean contains(float x, float y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        float x2 = (float) ((x - this.x) * cos(360.0 - angle) - (y - this.y) * sin(360.0 - angle));
+        float y2 = (float) ((x - this.x) * sin(360.0 - angle) + (y - this.y) * cos(360.0 - angle));
+        return shape.contains(x2, y2);
     }
 }
